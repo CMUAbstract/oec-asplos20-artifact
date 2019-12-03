27851a8966e174ceb0a3fabcafa84a647a15fa21
Brad Denby <bdenby@cmu.edu>
2019-12-03 12:14:28 -0500

    Updates license from CC0 Public Domain Dedication to Apache License Version 2.0

--- artifacts/sim-os/scripts/populate_gnd_ring.py
+++ artifacts/sim-os/scripts/populate_gnd_ring.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Usage: python3 populate_gnd_ring.py lat lon n id0 /path/to/dst/
 # Input:
 #  lat: latitude of all ground stations to be generated
@@ -8,8 +7,18 @@
 #  /path/to/dst/: Directory to place output files
 # Output:
 #  CSV file with [id,lat(deg),lon(deg),hae(km)]
-# Todo:
-#  Automatically determine height above ellipsoid (HAE) given (lat,lon)
+#
+# Copyright 2019 Bradley Denby
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at <http://www.apache.org/licenses/LICENSE-2.0>.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
 import csv
 import sys
--- artifacts/sim-os/scripts/populate_tx.py
+++ artifacts/sim-os/scripts/populate_tx.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Usage: python3 populate_tx.py /path/to/src/ ext power_w line_loss_db max_gain_db center_frequency_hz bandwidth_hz /path/to/dst/
 # Input:
 #  /path/to/src/: directory where the devices are read
@@ -11,6 +10,18 @@
 #  /path/to/dst/: directory where the files are written
 # Output:
 #  CSV file with [id,power(W),line_loss(dB),max_gain(dB),center_frequency(Hz),bandwidth(Hz)]
+#
+# Copyright 2019 Bradley Denby
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at <http://www.apache.org/licenses/LICENSE-2.0>.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
 
 import csv
 import os
